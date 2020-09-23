6d1a06d04446b2a80614fde6ec4f241ed143324a
Andrew Dillon <majik895@gmail.com>
2020-09-23 18:11:58 -0400

    passport hanging

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const express = require("express");
-const passport = require("passport");
-const GoogleStrategy = require("passport-google-oauth20");
-const keys = require("./config/keys");
+const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
 
 const app = express();
 
@@ -10,26 +10,27 @@ passport.use(
     {
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
-      callbackURL: "/auth/google/callback",
+      callbackURL: '/auth/google/callback',
     },
-    (accessToken) => {
-      console.log(accessToken);
+    (accessToken, refreshToken, profile, done) => {
+      console.log('access token', accessToken);
+      console.log('refresh token', refreshToken);
+      console.log('profile', profile);
     }
   )
 );
 
 app.get(
-  "/auth/google",
-  passport.authenticate("google", {
-    scope: ["profile", "email"],
+  '/auth/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email'],
   })
 );
 
+app.get('/auth/google/callback', passport.authenticate('google'));
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`\n xxx Server reporting for duty on port ${PORT} xxx \n`);
-});
+app.listen(PORT);
 
 // AIzaSyCmqPdLD4a36NREi8m-JgdbtqXrDpMyx5Q API Key
 // 1023486174794-4fn3ksqff9on69p5cio4lv4jjv26qmtn.apps.googleusercontent.com Client ID
